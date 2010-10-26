46562419b9eb46a6a6cb7fba0a011f6bbdb843c2
nagoya313 <nagoya313@gmail.com>
2010-10-27 01:08:51 +0900

    begins

---
+++ ngy313/detail/delete.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#pragma once
+#include <cassert>
+#include <memory>
+#include <Windows.h>
+#include <XAudio2.h>
+
+namespace ngy313 {
+namespace detial {
+struct handle_delete {
+  void operator ()(const HANDLE handle) const {
+    assert(handle && "ハンドルが不正");
+    CloseHandle(handle);
+  }
+};
+
+struct window_delete {
+  void operator ()(const HWND window_handle) const {
+    assert(window_handle && "ウィンドウハンドルが不正 ");
+    DestroyWindow(window_handle);
+  }
+};
+
+struct com_delete {
+  void operator ()(IUnknown * const ptr) {
+    assert(ptr && "COMポインタが不正");
+    ptr->Release();
+  }
+};
+
+struct mastering_voice_delete {
+  void operator ()(IXAudio2MasteringVoice * const voice) {
+    assert(voice && "マスターボイスポインタが不正");
+    voice->DestroyVoice();
+  }
+};
+}
+}
