f5bb1631059f83ac831f20bac054e754e1bd46f6
nagoya313 <nagoya313@gmail.com>
2010-11-14 23:26:48 +0900

    ベクトル型と演算を追加

--- ngy313/detail/math_expr.hpp
+++ ngy313/detail/math_expr.hpp
@@ -1,125 +1,132 @@
 #pragma once
 #include <boost/preprocessor/repeat.hpp>
+#include <boost/preprocessor/cat.hpp>
 
 namespace ngy313 { namespace expr { 
-struct add_t {
-  static float x(const float lhs, const float rhs) {
-    return lhs + rhs;
-  }
+#define NGY313_ELEM_MAX 4
 
-  static float y(const float lhs, const float rhs) {
-    return lhs + rhs;
-  }
+#define NGY313_VECTOR_PARAM (x)(y)(z)(w)
+
+#define NGY313_MEMBER2_GEN(z, n, data)\
+static float BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(const float lhs,\
+                                                       const float rhs) {\
+  return lhs data rhs;\
+}
+
+struct add_t {
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_MEMBER2_GEN, +)
 };
 
 struct sub_t {
-  static float x(const float lhs, const float rhs) {
-    return lhs - rhs;
-  }
-
-  static float y(const float lhs, const float rhs) {
-    return lhs - rhs;
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_MEMBER2_GEN, -)
 };
 
 struct mul_t {
-  static float x(const float lhs, const float rhs) {
-    return lhs * rhs;
-  }
-
-  static float y(const float lhs, const float rhs) {
-    return lhs * rhs;
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_MEMBER2_GEN, *)
 };
 
 struct div_t {
-  static float x(const float lhs, const float rhs) {
-    return lhs / rhs;
-  }
-
-  static float y(const float lhs, const float rhs) {
-    return lhs / rhs;
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_MEMBER2_GEN, /)
 };
 
-struct plus_t {
-  static float x(const float rhs) {
-    return rhs;
-  }
+#undef NGY313_MEMBER2_GEN
 
-  static float y(const float rhs) {
-    return rhs;
-  }
+#define NGY313_MEMBER1_GEN(z, n, data)\
+static float BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(const float rhs) {\
+  return BOOST_PP_CAT(data, rhs);\
+}
+
+struct plus_t {
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_MEMBER1_GEN, +)
 };
 
 struct minus_t {
-  static float x(const float rhs) {
-    return -rhs;
-  }
-
-  static float y(const float rhs) {
-    return -rhs;
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_MEMBER1_GEN, -)
 };
+
+#undef NGY313_MEMBER1_GEN
+
+#define NGY313_EXPR1_MEMBER_GEN(z, n, data)\
+float BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)() const {\
+  return Ope::BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(\
+             rhs_.BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)());\
+}
 
 template <typename Rhs, typename Ope>
 struct expr1 {
   explicit expr1(const Rhs &rhs) : rhs_(rhs) {}
 
-  float x() const {
-    return Ope::x(rhs_.x());
-  }
-
-  float y() const {
-    return Ope::y(rhs_.y());
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_EXPR1_MEMBER_GEN, _)
 
  private:
   const Rhs &rhs_;
 };
 
+#undef NGY313_EXPR1_MEMBER_GEN
+
+#define NGY313_EXPR_MEMBER_GEN(z, n, data)\
+float BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)() const {\
+  return Ope::BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(\
+             lhs_.BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(),\
+             rhs_.BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)());\
+}
+
 template <typename Lhs, typename Rhs, typename Ope>
 struct expr2 {
   expr2(const Lhs &lhs, const Rhs &rhs) : lhs_(lhs), rhs_(rhs) {}
 
-  float x() const {
-    return Ope::x(lhs_.x(), rhs_.x());
-  }
-
-  float y() const {
-    return Ope::y(lhs_.y(), rhs_.y());
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_EXPR_MEMBER_GEN, _)
 
  private:
   const Lhs &lhs_;
   const Rhs &rhs_;
 };
 
+#undef NGY313_EXPR_MEMBER_GEN
+
+#define NGY313_EXPR_RIGHT_FLOAT_MEMBER_GEN(z, n, data)\
+float BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)() const {\
+  return Ope::BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(\
+             lhs_.BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(),\
+             rhs_);\
+}
+
 template <typename Lhs, typename Ope>
 struct expr2<Lhs, float, Ope> {
   expr2(const Lhs &lhs, const float rhs) : lhs_(lhs), rhs_(rhs) {}
 
-  float x() const {
-    return Ope::x(lhs_.x(), rhs_);
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_EXPR_RIGHT_FLOAT_MEMBER_GEN, _)
 
  private:
   const Lhs &lhs_;
   const float rhs_;
 };
 
+#undef NGY313_EXPR_RIGHT_FLOAT_MEMBER_GEN
+
+#define NGY313_EXPR_LEFT_FLOAT_MEMBER_GEN(z, n, data)\
+float BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)() const {\
+  return Ope::BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)(\
+             lhs_,\
+             rhs_.BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)());\
+}
+
 template <typename Rhs, typename Ope>
 struct expr2<float, Rhs, Ope> {
   expr2(const float lhs, const Rhs &rhs) : lhs_(lhs), rhs_(rhs) {}
 
-  float x() const {
-    return Ope::x(lhs_, rhs_.x());
-  }
+  BOOST_PP_REPEAT(NGY313_ELEM_MAX, NGY313_EXPR_LEFT_FLOAT_MEMBER_GEN, _)
 
  private:
   const float slhs_;
   const Rhs &rhs_;
 };
+
+#undef NGY313_EXPR_LEFT_FLOAT_MEMBER_GEN
+
+#undef NGY313_VECTOR_PARAM
+
+#undef NGY313_ELEM_MAX
 
 template <typename Lhs, typename Rhs>
 expr2<Lhs, Rhs, add_t> operator +(const Lhs &lhs, const Rhs &rhs) {
---
+++ ngy313/vector.hpp
@@ -0,0 +1,95 @@
+#pragma once
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/repeat.hpp>
+#include <boost/preprocessor/repeat_from_to.hpp>
+#include <boost/preprocessor/seq/elem.hpp>
+#include <boost/preprocessor/enum_params.hpp>
+#include <d3dx9math.h>
+#include "detail/math_expr.hpp"
+
+namespace ngy313 { namespace expr {
+#define NGY313_VECTOR_PARAM (x)(y)(z)(w)
+
+#define NGY313_ACCESSER_GEN(z, n, data)\
+float &BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)() {\
+  return BOOST_PP_CAT(data, BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM));\
+}\
+\
+const float &BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)() const {\
+  return BOOST_PP_CAT(data, BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM));\
+}\
+
+#define NGY313_SUBSTITUTION_GEN(z, n, data)\
+pos_.BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM) data\
+    expr.BOOST_PP_SEQ_ELEM(n, NGY313_VECTOR_PARAM)();\
+
+
+#define NGY313_VECTOR_GEN(z, n, data)\
+struct BOOST_PP_CAT(vector, n) {\
+  BOOST_PP_CAT(vector, n)() : pos_() {}\
+\
+  BOOST_PP_CAT(vector, n)(BOOST_PP_ENUM_PARAMS(n, const float x))\
+      : pos_(BOOST_PP_ENUM_PARAMS(n, x)) {}\
+\
+  template <typename Expr>\
+  explicit BOOST_PP_CAT(vector, n)(const Expr &expr)\
+      : pos_(expr.x(), expr.y()) {}\
+\
+  template <typename Expr>\
+  BOOST_PP_CAT(vector, n) &operator =(const Expr &expr) {\
+    BOOST_PP_REPEAT(n, NGY313_SUBSTITUTION_GEN, =)\
+    return *this;\
+  }\
+\
+  template <typename Expr>\
+  BOOST_PP_CAT(vector, n) &operator +=(const Expr &expr) {\
+    BOOST_PP_REPEAT(n, NGY313_SUBSTITUTION_GEN, +=)\
+    return *this;\
+  }\
+\
+  template <typename Expr>\
+  BOOST_PP_CAT(vector, n) &operator -=(const Expr &expr) {\
+    BOOST_PP_REPEAT(n, NGY313_SUBSTITUTION_GEN, -=)\
+    return *this;\
+  }\
+\
+  BOOST_PP_CAT(vector, n) &operator *=(const float rhs) {\
+    pos_ *= rhs;\
+    return *this;\
+  }\
+\
+  BOOST_PP_CAT(vector, n) &operator /=(const float rhs) {\
+    pos_ /= rhs;\
+    return *this;\
+  }\
+\
+  bool operator ==(const BOOST_PP_CAT(vector, n) &rhs) const {\
+    return (pos_ == rhs.pos_) != 0;\
+  }\
+\
+  bool operator !=(const BOOST_PP_CAT(vector, n) &rhs) const {\
+    return (pos_ != rhs.pos_) != 0;\
+  }\
+\
+  BOOST_PP_REPEAT(n, NGY313_ACCESSER_GEN, pos_.)\
+\
+ private:\
+  BOOST_PP_CAT(D3DXVECTOR, n) pos_;\
+};
+
+BOOST_PP_REPEAT_FROM_TO(2, 5, NGY313_VECTOR_GEN, _)
+
+#undef NGY313_VECTOR_GEN
+
+#undef NGY313_SUBSTITUTION_GEN
+
+#undef NGY313_ACCESSER_GEN
+
+#undef NGY313_VECTOR_PARAM
+}}
+
+namespace ngy313 {
+using expr::vector2;
+using expr::vector3;
+using expr::vector4;
+}
