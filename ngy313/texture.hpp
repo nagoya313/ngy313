402b3f69ee3268cc04b9af1f0e4493bf0e96e25c
nagoya313 <nagoya313@gmail.com>
2010-12-12 14:05:01 +0900

    修正ミスを修正

--- ngy313/texture.hpp
+++ ngy313/texture.hpp
@@ -3,7 +3,6 @@
 #include "detail/texture.hpp"
 #include "drawable_base.hpp"
 #include "image.hpp"
-#include "vector.hpp"
 
 namespace ngy313 {
 class texture : public drawable_base<texture, 4, 2, image_2d_fvf_tag, triangle_strip_primitive_tag>,
@@ -13,12 +12,12 @@ class texture : public drawable_base<texture, 4, 2, image_2d_fvf_tag, triangle_s
       : drawable_base(init_vertex(x, y, img)), detail::texture_base(detail::drawable_core_access::texture1(img)) {}
 
  private:
-  static vertex_array_type init_vertex(const vector2 &pos, const image &img) {
+  static vertex_array_type init_vertex(const float x, const float y, const image &img) {
     const vertex_array_type vertex = {{
-      {{pos, 0.f, 1.f}, 0xFFFFFFFF, vector2(0.f, 0.f)},
-      {{vector2(pos.x() + img.width(), pos.y()), 0.f, 1.f}, 0xFFFFFFFF, vector2(1.f, 0.f)},
-      {{vector2(pos.x(), pos.y() + img.height()), 0.f, 1.f}, 0xFFFFFFFF, vector2(0.f, 1.f)},
-      {{vector2(pos.x() + img.width(), pos.y() + img.height()), 0.f, 1.f}, 0xFFFFFFFF, vector2(1.f, 1.f)}
+      {{x, y, 0.f, 1.f}, 0xFFFFFFFF, 0.f, 0.f},
+      {{x + img.width(), y, 0.f, 1.f}, 0xFFFFFFFF, 1.f, 0.f},
+      {{x, y + img.height(), 0.f, 1.f}, 0xFFFFFFFF, 0.f, 1.f},
+      {{x + img.width(), y + img.height(), 0.f, 1.f}, 0xFFFFFFFF, 1.f, 1.f}
     }};
     return vertex;
   }
