c39a77049009ab8ccaba738191737582c3415196
nagoya313 <nagoya313@gmail.com>
2010-12-12 12:51:09 +0900

    余計なコメントアウトを削除

--- ngy313/texture.hpp
+++ ngy313/texture.hpp
@@ -23,24 +23,3 @@ class texture : public drawable_base<texture, 4, 2, image_2d_fvf_tag, triangle_s
     return vertex;
   }
 };
-
-/*
-class texture3d : public drawable_base<texture3d, 4, 2, image_3d_fvf_tag, triangle_strip_primitive_tag>,
-                  public detail::texture_base {
- public:
-  texture3d(const vector3 &pos, const image &img)
-      : drawable_base(init_vertex(pos, img)), detail::texture_base(detail::drawable_core_access::texture1(img)) {}
-
- private:
-  static vertex_array_type init_vertex(const vector3 &pos, const image &img) {
-    const vertex_array_type vertex = {{
-      {{pos.x(), pos.y(), 0.f}, 0xFFFFFFFF, {0.f, 0.f}},
-      {{pos.x() + img.width(), pos.y(), 0.f}, 0xFFFFFFFF, {1.f, 0.f}},
-      {{pos.x(), pos.y() + img.height(), 0.f}, 0xFFFFFFFF, {0.f, 1.f}},
-      {{pos.x() + img.width(), pos.y() + img.height(), 0.f}, 0xFFFFFFFF, {1.f, 1.f}}
-    }};
-    return vertex;
-  }
-};
-*/
-} 
