3808c72d0d8380f0233c4ea12d3071a6d180b6e5
nagoya313 <nagoya313@gmail.com>
2011-02-10 12:17:14 +0900

    不要ファイルの削除

--- ngy313/drawable_adaptor.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "detail/drawable_adaptor.hpp"
-
-namespace ngy313 {
-using detail::adaptor_result;
-using detail::argument_result;
-using detail::vertex_adaptor;
-}
--- ngy313/drawable_base.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "detail/drawable.hpp"
-#include "fvf_tag.hpp"
-#include "primitive_tag.hpp"
-
-namespace ngy313 {
-using detail::drawable_base;
-}
