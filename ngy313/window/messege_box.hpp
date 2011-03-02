e88493a1e9385214b169ff0d8473743d33b2cd18
nagoya313 <nagoya313@gmail.com>
2011-03-03 00:39:37 +0900

    不要関数を削除

--- ngy313/window/messege_box.hpp
+++ ngy313/window/messege_box.hpp
@@ -2,22 +2,10 @@
 #include <ngy313/window/detail/message_box.hpp>
 
 namespace ngy313 { namespace window {
-class main_window;
-
 inline
 void message_box(const utility::string_piece &message, 
                  const utility::string_piece &caption,
                  const icon_code icon) {
   detail::message_box(message, caption, icon);
-}
-
-// インスタンスベース版も作ったほうがいいかもしれない
-inline
-void message_box(const main_window &main_wnd,
-                 const utility::string_piece &message, 
-                 const utility::string_piece &caption,
-                 const icon_code icon) {
-  // 未実装
-  //detail::message_box(message, caption, icon);
 }
 }}
