42e4ad121de0bb024cef1677b859cbc5ce668306
nagoya313 <nagoya313@gmail.com>
2011-07-20 21:31:43 +0900

    operator |のオーバーロードを追加

---
+++ ngy313/pipe_operator.hpp
@@ -0,0 +1,29 @@
+#ifndef NGY313_PIPE_OPERATOR_HPP_
+#define NGY313_PIPE_OPERATOR_HPP_
+
+#include <utility>
+
+#if defined(_MSV_VER)
+#include <boost/utility/declval.hpp>
+#endif
+
+namespace ngy313 { namespace pipe_operator {
+#if defined(_MSC_VER)
+template <typename Base, typename Adaptor>
+decltype(detail::declval<Adaptor>()(boost::declval<Base>()))
+    operator |(Base &&base, Adaptor &&adaptor) {
+  return std::forward<Adaptor>(adaptor)(std::forward<Base>(base));
+}
+#else
+template <typename Base, typename Adaptor>
+decltype(std::declval<Adaptor>()(std::declval<Base>())) 
+    operator |(Base &&base, Adaptor &&adapter) {
+  return std::forward<Adaptor>(adapter)(std::forward<Base>(base));
+}
+#endif
+
+template <typename Adaptor>
+struct base {};
+}}
+
+#endif
