1d7f68fb6352a51e6a1c1026294e398841b33272
nagoya313 <nagoya313@gmail.com>
2011-07-20 21:12:14 +0900

    環境分岐ヘッダを追加

---
+++ ngy313/platform.hpp
@@ -0,0 +1,21 @@
+#ifndef NGY313_PLATFORM_HPP_
+#define NGY313_PLATFORM_HPP_
+
+#if defined(_WIN32)
+#if !(defined(_MSC_VER) && (_MSC_VER >= 1600))
+#error It is a compiler of non-correspondence.
+#endif
+#define _WIN32_DCOM
+#define _WIN32_WINNT 0x501
+#define NOMINMAX
+#endif
+#elif defined(__linux__)
+#if !(defined(__GNUC__) && (__GNUC__ >= 4) && (__GNUC_MINOR__ >= 6))
+#error It is a compiler of non-correspondence.
+#endif
+#else
+#error It is an environment that does not correspond.
+#endif
+
+#endif
+
