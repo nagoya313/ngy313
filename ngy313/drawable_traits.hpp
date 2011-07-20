915d01ae7be9512fc8b231441c55265ce95a3449
nagoya313 <nagoya313@gmail.com>
2011-07-20 21:50:04 +0900

    描画オブジェクトの基底クラス群を追加

---
+++ ngy313/drawable_traits.hpp
@@ -0,0 +1,62 @@
+#ifndef NGY313_DRAWABLE_TRAITS_HPP_
+#define NGY313_DRAWABLE_TRAITS_HPP_
+
+#include <cstdint>
+#include <array>
+#include <type_traits>
+#include <boost/mpl/contains.hpp>
+#include <boost/mpl/insert.hpp>
+#include <boost/mpl/map.hpp>
+#include <ngy313/fvf_traits.hpp>
+#include <ngy313/types.hpp>
+#include <ngy313/detail/drawable_traits.hpp>
+
+namespace ngy313 {
+template <typename Drawable, 
+          std::size_t Size,
+          std::uint32_t Count,
+          typename FVFTag, 
+          typename PrimitiveTag>
+struct drawable_traits {
+  static_assert(boost::mpl::contains<detail::primitive_type::tag_list,
+                                     PrimitiveTag>::value, "");
+  
+  typedef boost::mpl::map<
+      boost::mpl::pair<detail::drawable_key, Drawable>, 
+      boost::mpl::pair<detail::size_key, 
+                       std::integral_constant<std::size_t, Size>>,
+      boost::mpl::pair<detail::count_key, 
+                       std::integral_constant<std::uint32_t, Count>>,
+      boost::mpl::pair<detail::fvf_key, FVFTag>,
+      boost::mpl::pair<detail::primitive_key, PrimitiveTag>> list_type;
+
+  typedef typename boost::mpl::at<list_type,
+                                  detail::size_key>::type size_type;
+  typedef typename boost::mpl::at<list_type,
+                                  detail::count_key>::type count_type;
+  typedef typename boost::mpl::at<list_type,
+                                  detail::fvf_key>::type fvf_type;
+  typedef typename boost::mpl::at<list_type,
+                                  detail::primitive_key>::type primitive_type;
+
+  typedef typename fvf_traits<FVFTag>::vertex_type vertex_type; 
+  typedef std::array<vertex_type, Size> vertex_array_type;
+};
+
+template <typename Drawable>
+struct copy_drawable {
+  typedef typename Drawable::list_type list_type;
+  typedef typename Drawable::vertex_type vertex_type;
+  typedef typename Drawable::vertex_array_type vertex_array_type;
+};
+
+template <typename Drawable, typename AddData>
+struct add_drawable {
+  typedef typename boost::mpl::insert<typename Drawable::list_type,
+                                      AddData>::type list_type;
+  typedef typename Drawable::vertex_type vertex_type;
+  typedef typename Drawable::vertex_array_type vertex_array_type;
+};
+}
+
+#endif
