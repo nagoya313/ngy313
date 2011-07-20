a0bd589ce7a24e89ade7d3a615ce3e47279013ca
nagoya313 <nagoya313@gmail.com>
2011-07-21 01:50:18 +0900

    optionalを使った実装に変更

--- ngy313/detail/main_singleton.hpp
+++ ngy313/detail/main_singleton.hpp
@@ -2,6 +2,15 @@
 #define NGY313_DETAIL_MAIN_SINGLETON_HPP_
 
 #include <boost/noncopyable.hpp>
+#include <boost/optional.hpp>
+
+#include <boost/config.hpp>
+
+//#if defined(BOOST_NO_0X_HDR_THREAD)
+//#include <boost/thread.hpp>
+//#else
+#include <thread>
+//#endif
 
 namespace ngy313 { namespace detail {
 template <typename Main,
@@ -16,25 +25,54 @@ class basic_main_singleton : boost::noncopyable {
   }
 
   Main &main() {
-    return main_;
+//#if defined(BOOST_NO_0X_HDR_THREAD)
+  	//static boost::once_flag flag = BOOST_ONCE_INIT;
+  	//boost::call_once([this] {main_init();}, flag);
+//#else
+  	static std::once_flag flag;
+  	std::call_once(flag, [this] {this->main_init();});
+//#endif
+    return *main_;
   }
 
   Window &window() {
-    return window_;
+//#if defined(BOOST_NO_0X_HDR_THREAD)
+  	//static boost::once_flag flag = BOOST_ONCE_INIT;
+  	//boost::call_once([this] {window_init();}, flag);
+//#else
+  	static std::once_flag flag;
+  	std::call_once(flag, [this] {this->window_init();});
+//#endif
+    return *window_;
   }
 
   Graphic &graphic() {
-    return graphic_;
+//#if defined(BOOST_NO_0X_HDR_THREAD)
+  	//static boost::once_flag flag = BOOST_ONCE_INIT;
+  	//boost::call_once([this] {graphic_init();}, flag);
+//#else
+  	static std::once_flag flag;
+  	std::call_once(flag, [this] {this->graphic_init();});
+//#endif
+    return *graphic_;
   }
 
   Sound &sound() {
-    return sound_;
+//#if defined(BOOST_NO_0X_HDR_THREAD)
+  	//static boost::once_flag flag = BOOST_ONCE_INIT;
+  	//boost::call_once([this] {sound_init();}, flag);
+//#else
+  	static std::once_flag flag;
+  	std::call_once(flag, [this] {this->sound_init();});
+//#endif
+    return *sound_;
   }
 
  private:
   basic_main_singleton() : main_(), 
                            window_(),
-                           graphic_(window_) {}
+                           graphic_(),
+                           sound_() {}
 
   static Window init_window() {
     Window window;
@@ -44,10 +82,26 @@ class basic_main_singleton : boost::noncopyable {
     return window;
   }
 
-  Main main_;
-  Window window_;
-  Graphic graphic_;
-  Sound sound_;
+  void main_init() {
+  	main_ = boost::in_place();
+  }
+
+  void window_init() {
+  	window_ = boost::in_place();
+  }
+
+  void graphic_init() {
+    graphic_ = boost::in_place(window());
+  }
+
+  void sound_init() {
+   sound_ = boost::in_place();
+  }
+
+  boost::optional<Main> main_;
+  boost::optional<Window> window_;
+  boost::optional<Graphic> graphic_;
+  boost::optional<Sound> sound_;
 };
 }}
 
