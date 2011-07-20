657a20713269d4642e8579a377311cc1b76a93a5
nagoya313 <nagoya313@gmail.com>
2011-07-20 21:35:40 +0900

    前方宣言を追加

---
+++ ngy313/fwd.hpp
@@ -0,0 +1,15 @@
+#ifndef NGY313_FWD_HPP_
+#define NGY313_FWD_HPP_
+
+namespace ngy313 {
+template <typename Code>
+class basic_color;
+
+template <typename Icon>
+class basic_icon;
+
+template <typename Texture>
+class basic_texture;
+}
+
+#endif
