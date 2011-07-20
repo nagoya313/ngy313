0cdb25920020059c6da9e7cdf3094eaf96f7232f
nagoya313 <nagoya313@gmail.com>
2011-07-20 23:10:09 +0900

    色の型変換に関する函数群を追加

---
+++ ngy313/detail/color.hpp
@@ -0,0 +1,37 @@
+#ifndef NGY313_DETAIL_COLOR_HPP_
+#define NGY313_DETAIL_COLOR_HPP_
+
+#include <cstdint>
+#include <boost/config.hpp>
+
+namespace ngy313 { namespace detail {
+BOOST_CONSTEXPR float uint8_to_float(std::uint8_t code) {
+  return static_cast<float>(code) / 255.f;
+}
+
+BOOST_CONSTEXPR float uint32_to_float(std::uint32_t code,
+                                      std::uint32_t shift) {
+  return static_cast<float>(
+             static_cast<std::uint8_t>(code >> shift)) / 255.f;
+}
+
+BOOST_CONSTEXPR std::uint8_t float_to_uint8(float f) {
+  return f >= 1.f ?
+         0xFF :
+         f <= 0.f ?
+         0x00 :
+         static_cast<std::uint8_t>(f * 255.f + 0.5f);
+}
+
+BOOST_CONSTEXPR std::uint32_t float_to_uint32(float f,
+                                              std::uint32_t shift) {
+  return static_cast<std::uint32_t>(
+            f >= 1.f ?
+            0xFF :
+            f <= 0.f ?
+            0x00 :
+            static_cast<std::uint32_t>(f * 255.f + 0.5f)) << shift;
+}
+}}
+
+#endif
