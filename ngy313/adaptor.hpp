0048fddaad5917c8e3f037d6ee88e78ee3133d3b
nagoya313 <nagoya313@gmail.com>
2011-07-20 23:57:24 +0900

    アダプタ用の基底クラスをアダプタ側に統合

--- ngy313/adaptor.hpp
+++ ngy313/adaptor.hpp
@@ -2,16 +2,21 @@
 #define NGY313_ADAPTOR_HPP_
 
 #include <type_traits>
+#include <boost/mpl/insert.hpp>
 #include <boost/fusion/include/at_key.hpp>
 #include <ngy313/drawable_traits.hpp>
 
 namespace ngy313 {
 template <typename Adaptor, typename Drawable>
-struct drawable_adaptor : copy_drawable<Drawable> {
+struct drawable_adaptor {
+	typedef typename Drawable::list_type list_type;
+	typedef typename Drawable::vertex_type vertex_type;
+	typedef typename Drawable::vertex_array_type vertex_array_type;
+
   explicit drawable_adaptor(const Drawable &drawable)
       : list_(detail::drawable_fusion_initializer::init(drawable)) {}
 
-  typename Drawable::vertex_array_type vertex() const {
+  vertex_array_type vertex() const {
     auto vertex = boost::fusion::at_key<detail::drawable_key>(list_).vertex();
     static_cast<const Adaptor &>(*this).transform(vertex);
     return vertex;
@@ -25,12 +30,17 @@ struct drawable_adaptor : copy_drawable<Drawable> {
   const typename detail::drawable_fusion<Drawable>::type list_;
 };
 
-template <typename Drawable, typename Add>
-struct add_drawable_adaptor : add_drawable<Drawable, Add> {
+template <typename Drawable, typename AddData>
+struct add_drawable_adaptor {
+	typedef typename boost::mpl::insert<typename Drawable::list_type,
+		                                  AddData>::type list_type;
+	typedef typename Drawable::vertex_type vertex_type;
+	typedef typename Drawable::vertex_array_type vertex_array_type;
+
   explicit add_drawable_adaptor(const Drawable &drawable)
       : list_(detail::drawable_fusion_initializer::init(drawable)) {}
 
-  typename Drawable::vertex_array_type vertex() const {
+  vertex_array_type vertex() const {
     return boost::fusion::at_key<detail::drawable_key>(list_).vertex();
   }
 
