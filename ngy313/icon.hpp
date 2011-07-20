4dba2220e66727e1e6f9a2ececc09b9835107edc
nagoya313 <nagoya313@gmail.com>
2011-07-20 21:40:36 +0900

    アイコンクラステンプレートを追加

---
+++ ngy313/icon.hpp
@@ -0,0 +1,44 @@
+#ifndef NGY313_ICON_HPP_
+#define NGY313_ICON_HPP_
+
+#include <boost/noncopyable.hpp>
+#include <ngycl/string_piece.hpp>
+#include <ngycl/detail/ngycl.hpp>
+
+#if defined(_WIN32)
+#include <ngy313/detail/win32_icon.hpp>
+#elif defined(__linux__)
+#include <ngy313/detail/gtkmm_icon.hpp>
+#endif
+
+namespace ngy313 { namespace detail {
+struct init_base {
+	init_base() {
+		detail::main_singleton::instance();
+	}
+};
+}}
+
+namespace ngy313 {
+template <typename Icon>
+class basic_icon : detail::init_base, boost::noncopyable {
+ public:
+  explicit basic_icon(const string_piece &file_name) 
+      : icon_(file_name) {}
+      
+  typename Icon::handle_type handle() const {
+    return icon_.handle();
+  }
+  
+ private:
+  const Icon icon_;
+};
+
+#if defined(_WIN32)
+typedef basic_icon<detail::win32_icon> icon; 
+#elif defined(__linux__)
+typedef basic_icon<detail::gtkmm_icon> icon;
+#endif
+}
+
+#endif
