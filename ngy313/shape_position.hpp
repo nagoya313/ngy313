ba0feecb4cb9879ba7cd0c8c41a38f2962631249
nagoya313 <nagoya313@gmail.com>
2011-07-22 00:55:58 +0900

    uv座標を扱えるように拡張

--- ngy313/shape_position.hpp
+++ ngy313/shape_position.hpp
@@ -2,6 +2,7 @@
 #define NGY313_SHAPE_POSITION_HPP_
 
 #include <cstddef>
+#include <boost/config.hpp>
 #include <boost/mpl/at.hpp>
 #include <boost/range/numeric.hpp>
 #include <ngy313/fvf_traits.hpp>
@@ -29,6 +30,22 @@ const struct add_position_z_t {
     return lhs + vertex_member_at<position_member>(vertex).z_;
   }
 } add_position_z = {};
+
+template <std::size_t Index>
+struct add_position_u_t {
+  template <typename Vertex>
+  float operator ()(float lhs, const Vertex &vertex) const {
+    return lhs + vertex_member_at<texture_member>(vertex).texture_array[Index].u;
+  }
+};
+
+template <std::size_t Index>
+struct add_position_v_t {
+  template <typename Vertex>
+  float operator ()(float lhs, const Vertex &vertex) const {
+    return lhs + vertex_member_at<texture_member>(vertex).texture_array[Index].v;
+  }
+};
 }}
 
 namespace ngy313 {
@@ -42,6 +59,18 @@ float shape_position_y(const Drawable &drawable, std::size_t at) {
   return vertex_member_at<position_member>(drawable.vertex()[at]).y_;
 }
 
+template <typename Drawable, std::size_t Index>
+float shape_position_u(const Drawable &drawable, std::size_t at) {
+  return vertex_member_at<texture_member>(
+             drawable.vertex()[at]).texture_array[Index].u;
+}
+
+template <typename Drawable, std::size_t Index>
+float shape_position_v(const Drawable &drawable, std::size_t at) {
+  return vertex_member_at<texture_member>(
+             drawable.vertex()[at]).texture_array[Index].v;
+}
+
 template <typename Drawable>
 float shape_center_x(const Drawable &drawable) {
   return boost::accumulate(drawable.vertex(), 0.f, detail::add_position_x) /
@@ -53,6 +82,26 @@ float shape_center_x(const Drawable &drawable) {
 template <typename Drawable>
 float shape_center_y(const Drawable &drawable) {
   return boost::accumulate(drawable.vertex(), 0.f, detail::add_position_y) /
+       static_cast<float>(boost::mpl::at<
+     		                      typename Drawable::list_type,
+     		                      detail::size_key>::type::type::value);
+}
+
+template <std::size_t Index, typename Drawable>
+float shape_center_u(const Drawable &drawable) {
+  return boost::accumulate(drawable.vertex(), 
+                           0.f,
+                           detail::template add_position_u_t<Index>()) /
+      static_cast<float>(boost::mpl::at<
+      		                     typename Drawable::list_type,
+      		                     detail::size_key>::type::type::value);
+}
+
+template <std::size_t Index, typename Drawable>
+float shape_center_v(const Drawable &drawable) {
+  return boost::accumulate(drawable.vertex(),
+                           0.f,
+                           detail::template add_position_v_t<Index>()) /
        static_cast<float>(boost::mpl::at<
      		                      typename Drawable::list_type,
      		                      detail::size_key>::type::type::value);
