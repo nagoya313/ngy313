3dd839f9e458ae2bd8f6fdbddf8ee464103dcc94
nagoya313 <nagoya313@gmail.com>
2011-07-21 23:55:00 +0900

    コード整形

--- ngy313/sprite.hpp
+++ ngy313/sprite.hpp
@@ -7,8 +7,8 @@
 
 namespace ngy313 {
 class sprite : public drawable_traits<sprite,
-                                           4,
-                                           2,
+                                      4,
+                                      2,
                                       image_2d_fvf_tag,
                                       triangle_strip_primitive_tag> {
  public:
@@ -28,13 +28,17 @@ class sprite : public drawable_traits<sprite,
   static vertex_array_type init(const texture &tex) {
     const vertex_array_type vertex = {{
       vertex_type(rhw_position_t(vector2(0.f, 0.f)),
-      		         texture_t<1>(vector2(0.f, 0.f))),
-      vertex_type(rhw_position_t(vector2(static_cast<float>(tex.width()), 0.f)),
-      		         texture_t<1>(vector2(1.f, 0.f))),
-      vertex_type(rhw_position_t(vector2(0.f, static_cast<float>(tex.height()))),
-      		         texture_t<1>(vector2(0.f, 1.f))),
-      vertex_type(rhw_position_t(vector2(static_cast<float>(tex.width()), static_cast<float>(tex.height()))),
-      		         texture_t<1>(vector2(1.f, 1.f)))
+      		        texture_t<1>(vector2(0.f, 0.f))),
+      vertex_type(rhw_position_t(
+                      vector2(static_cast<float>(tex.width()), 0.f)),
+      		        texture_t<1>(vector2(1.f, 0.f))),
+      vertex_type(rhw_position_t(
+                      vector2(0.f, static_cast<float>(tex.height()))),
+      		        texture_t<1>(vector2(0.f, 1.f))),
+      vertex_type(rhw_position_t(
+                  vector2(static_cast<float>(tex.width()),
+                          static_cast<float>(tex.height()))),
+      		        texture_t<1>(vector2(1.f, 1.f)))
      }};
     return vertex;
    }
@@ -65,17 +69,21 @@ class bright_sprite : public drawable_traits<bright_sprite,
   static vertex_array_type init(const texture &tex) {
     const vertex_array_type vertex = {{
       vertex_type(rhw_position_t(vector2(0.f, 0.f)),
-      		         diffuse_t(),
-      		         texture_t<1>(vector2(0.f, 0.f))),
-      vertex_type(rhw_position_t(vector2(static_cast<float>(tex.width()), 0.f)),
-      		         diffuse_t(),
-      		         texture_t<1>(vector2(1.f, 0.f))),
-      vertex_type(rhw_position_t(vector2(0.f, static_cast<float>(tex.height()))),
-      		         diffuse_t(),
-      		         texture_t<1>(vector2(0.f, 1.f))),
-      vertex_type(rhw_position_t(vector2(static_cast<float>(tex.width()), static_cast<float>(tex.height()))),
-      		         diffuse_t(),
-      		         texture_t<1>(vector2(1.f, 1.f)))
+      		        diffuse_t(),
+      		        texture_t<1>(vector2(0.f, 0.f))),
+      vertex_type(rhw_position_t(
+                      vector2(static_cast<float>(tex.width()), 0.f)),
+      		        diffuse_t(),
+      		        texture_t<1>(vector2(1.f, 0.f))),
+      vertex_type(rhw_position_t(
+                      vector2(0.f, static_cast<float>(tex.height()))),
+      		        diffuse_t(),
+      		        texture_t<1>(vector2(0.f, 1.f))),
+      vertex_type(rhw_position_t(
+                      vector2(static_cast<float>(tex.width()),
+                              static_cast<float>(tex.height()))),
+      		        diffuse_t(),
+      		        texture_t<1>(vector2(1.f, 1.f)))
     }};
     return vertex;
   }
@@ -121,15 +129,15 @@ class size_sprite : public drawable_traits<size_sprite,
   texture texture_;
 };
 
-class bright_size_sprite : public drawable_traits<bright_size_sprite,
-                                                  4,
-                                                  2,
-                                                  bright_image_2d_fvf_tag,
-                                                  triangle_strip_primitive_tag> {
+class bright_size_sprite
+    : public drawable_traits<bright_size_sprite,
+                             4,
+                             2,
+                             bright_image_2d_fvf_tag,
+                             triangle_strip_primitive_tag> {
  public:
   template <typename Image>
-  explicit bright_size_sprite(const vector2 &size,
-                              const Image &image)
+  explicit bright_size_sprite(const vector2 &size, const Image &image)
       : vertex_(init(size)), texture_(image) {}
 
   vertex_array_type vertex() const {
