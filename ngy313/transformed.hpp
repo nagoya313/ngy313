a94230998f773744fa86ed3d4a70b0b0c726abc7
nagoya313 <nagoya313@gmail.com>
2011-07-21 23:57:24 +0900

    移動実装部にvector2を取る+=を使うように変更

--- ngy313/transformed.hpp
+++ ngy313/transformed.hpp
@@ -18,8 +18,7 @@ struct adaptor : drawable_adaptor<adaptor<Drawable>, Drawable> {
 
   template <typename VertexArray>
   void transform(VertexArray &vertex) const {
-  	vertex_member_at<position_member>(vertex[at_]).x_ += move_.get_x();
-  	vertex_member_at<position_member>(vertex[at_]).y_ += move_.get_y();
+  	vertex_member_at<position_member>(vertex[at_]).x_ += move_;
   }
 
  private:
