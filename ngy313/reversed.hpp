0888d68201fd38b6df40ca39ddd00669bbf0b6fb
nagoya313 <nagoya313@gmail.com>
2011-07-22 15:08:27 +0900

    反転アダプタを追加

---
+++ ngy313/reversed.hpp
@@ -0,0 +1,18 @@
+#ifndef NGY313_REVERSED_HPP_
+#define NGY313_REVERSED_HPP_
+
+#include <ngy313/extended.hpp>
+#include <ngy313/base_point.hpp>
+
+namespace ngy313 {
+template <typename Drawable>
+extended_adaptor::detail::adaptor<Drawable> make_reversed(const Drawable &drawable) {
+  return extended_adaptor::detail::adaptor<Drawable>(drawable, 
+                                                     base_point_set_center,
+                                                     vector2(-1.f, 1.f));
+}
+
+BOOST_CONSTEXPR auto reversed = extended(base_point_set_center, vector2(-1.f, 1.f));
+}
+
+#endif
