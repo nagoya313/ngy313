64f98c4dd94bb0bacaee649a258414bee660f93d
nagoya313 <nagoya313@gmail.com>
2011-07-23 15:11:31 +0900

    詳細に関する前方宣言を追加

---
+++ ngy313/detail/fwd.hpp
@@ -0,0 +1,12 @@
+#ifndef NGY313_DETAIL_FWD_HPP_
+#define NGY313_DETAIL_FWD_HPP_
+
+namespace ngy313 { namespace detail {
+template <typename Window>
+class basic_window;
+
+template <typename Device>
+class basic_graphic_system;
+}}
+
+#endif
