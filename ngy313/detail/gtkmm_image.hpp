333466e5dea019aa5abcac8efb8af763729cddfd
nagoya313 <nagoya313@gmail.com>
2011-07-24 15:15:57 +0900

    画像読み込みの仕様変更に合わせて修正

--- ngy313/detail/gtkmm_image.hpp
+++ ngy313/detail/gtkmm_image.hpp
@@ -5,42 +5,65 @@
 #include <type_traits>
 #include <gtkmm.h>
 #include <ngy313/string_wrap.hpp>
+#include <ngy313/detail/image.hpp>
+#include <boost/gil/gil_all.hpp>
+#include <boost/gil/extension/io/png_io.hpp>
 
 namespace ngy313 { namespace detail {
 template <typename Texture>
-class gtkmm_image {
- public:
-	explicit gtkmm_image(const string_wrap &file_name)
+class gtkmm_image : public image_base<Texture> {
+  class loader {
+   public:
+    explicit loader(const string_wrap &file_name)
 	    : file_name_(file_name.string()) {}
 
-  template <typename Device,
-            typename Result = typename Texture::texture_tuple,
-            typename Handle = typename std::remove_reference<
-                                  typename Texture::handle_type>::type,
-            typename Deleter = typename Texture::deleter_type>
-  Result operator ()(const Device &device) const {
-    try {
-      const Glib::RefPtr<Gdk::Pixbuf> pixel(
-  		  	Gdk::Pixbuf::create_from_file(file_name_.c_str()));
-      const Handle id(new GLuint(), Deleter(device));
-      glEnable(GL_TEXTURE_2D);
-      glGenTextures(1, id.get());
-      glBindTexture(GL_TEXTURE_2D, *id);
-      gluBuild2DMipmaps(GL_TEXTURE_2D,
-                           3,
-                        pixel->get_width(),
-                        pixel->get_height(),
-                        GL_RGBA,
-                        GL_UNSIGNED_BYTE,
-                        pixel->get_pixels());
-      return Result(id, pixel->get_width(), pixel->get_height());
-  	} catch(const Glib::Error &error) {
-  	  throw std::runtime_error("画像の読み込みに失敗しました");
-  	}
+   template <typename Device,
+                    typename Result = typename Texture::texture_tuple,
+                    typename Handle = typename std::remove_reference<
+                                                       typename Texture::handle_type>::type,
+                    typename Deleter = typename Texture::deleter_type>
+    Result operator ()(const Device &device) const {
+      try {
+        const Glib::RefPtr<Gdk::Pixbuf> pixel(
+  	  	  	Gdk::Pixbuf::create_from_file(file_name_.c_str()));
+        const Handle id(new GLuint(), Deleter(device));
+        glEnable(GL_TEXTURE_2D);
+        glGenTextures(1, id.get());
+        glBindTexture(GL_TEXTURE_2D, *id);
+        gluBuild2DMipmaps(GL_TEXTURE_2D,
+                                    GL_RGBA,
+                                    pixel->get_width(),
+                                    pixel->get_height(),
+                                    GL_RGBA,
+                                    GL_UNSIGNED_BYTE,
+                                    pixel->get_pixels());
+        return Result(id, pixel->get_width(), pixel->get_height());
+     } catch(const Glib::Error &error) {
+  	throw std::runtime_error("画像の読み込みに失敗しました");
+     }
+   }
+
+   private:
+    std::string file_name_;
+  };
+ public:
+  explicit gtkmm_image(const string_wrap &file_name)
+	    : texture_(loader(file_name)) {}
+
+  virtual int width() const {
+    return texture_.width();
+  }
+
+  virtual int height() const {
+    return texture_.height();
+  }
+
+  virtual const Texture &get_texture() const {
+    return texture_;
   }
 
  private:
-  std::string file_name_;
+  Texture texture_;
 };
 }}
 
