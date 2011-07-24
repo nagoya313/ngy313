bf0a4554868f0dcc31b7b4cdc10bfce937b53f89
nagoya313 <nagoya313@gmail.com>
2011-07-24 15:26:38 +0900

    コード整形

--- ngy313/detail/opengl_texture.hpp
+++ ngy313/detail/opengl_texture.hpp
@@ -37,13 +37,15 @@ texture_tuple create_empty_texture(const Device &device,
   glBindTexture(GL_TEXTURE_2D, *id);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexImage2D(GL_TEXTURE_2D, 0,
-                                    GL_RGBA,
-                                    width,
-                                    height, 0,
-                                    GL_RGBA,
-                                    GL_UNSIGNED_BYTE,
-                                    nullptr);
+  glTexImage2D(GL_TEXTURE_2D, 
+                   0,
+               GL_RGBA,
+               width,
+               height, 
+                   0,
+               GL_RGBA,
+               GL_UNSIGNED_BYTE,
+               nullptr);
   return texture_tuple(id, width, height);
 }
 
