1e6dc25b0faedc45be364dffdf1502685a04b11f
nagoya313 <nagoya313@gmail.com>
2011-07-24 15:29:11 +0900

    レンダリングターゲットのLinux-OpenGL設定を追加

--- ngy313/render_target.hpp
+++ ngy313/render_target.hpp
@@ -8,6 +8,7 @@
 #if defined(_WIN32)
 #include <ngy313/detail/direct3d9_render_target.hpp>
 #elif defined(__linux__)
+#include <ngy313/detail/opengl_render_target.hpp>
 #endif
 
 namespace ngy313 {
@@ -60,6 +61,9 @@ typedef basic_render_target<detail::direct3d9_render_target<texture>> render_tar
 typedef basic_scoped_render_target<detail::direct3d9_scoped_render_target<render_target>> 
     scoped_render_target;
 #elif defined(__linux__)
+typedef basic_render_target<detail::opengl_render_target<texture>> render_target;
+typedef basic_scoped_render_target<detail::opengl_scoped_render_target<render_target>> 
+    scoped_render_target;
 #endif
 }
 
