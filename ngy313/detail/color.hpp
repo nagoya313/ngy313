ae61bfea10f6a9ab6578191d16e4770d84200d32
nagoya313 <nagoya313@gmail.com>
2011-07-26 03:39:52 +0900

    inlineの付け忘れの修正と色を0～1の間に落とすユーティリティを追加

--- ngy313/detail/color.hpp
+++ ngy313/detail/color.hpp
@@ -2,9 +2,11 @@
 #define NGY313_DETAIL_COLOR_HPP_
 
 #include <cstdint>
+#include <algorithm>
 #include <boost/config.hpp>
 
 namespace ngy313 { namespace detail {
+inline
 BOOST_CONSTEXPR float uint8_to_float(std::uint8_t code) {
   return static_cast<float>(code) / 255.f;
 }
@@ -15,6 +17,7 @@ BOOST_CONSTEXPR float uint32_to_float(std::uint32_t code,
              static_cast<std::uint8_t>(code >> shift)) / 255.f;
 }
 
+inline
 BOOST_CONSTEXPR std::uint8_t float_to_uint8(float f) {
   return f >= 1.f ?
          0xFF :
@@ -23,6 +26,7 @@ BOOST_CONSTEXPR std::uint8_t float_to_uint8(float f) {
          static_cast<std::uint8_t>(f * 255.f + 0.5f);
 }
 
+inline
 BOOST_CONSTEXPR std::uint32_t float_to_uint32(float f,
                                               std::uint32_t shift) {
   return static_cast<std::uint32_t>(
@@ -32,6 +36,11 @@ BOOST_CONSTEXPR std::uint32_t float_to_uint32(float f,
             0x00 :
             static_cast<std::uint32_t>(f * 255.f + 0.5f)) << shift;
 }
+
+inline
+float limit(float value) {
+  return std::max(std::min(value, 1.f), 0.f);
+} 
 }}
 
 #endif
