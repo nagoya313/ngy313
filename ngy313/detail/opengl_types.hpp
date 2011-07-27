fbaf17c694c5e8546f023f5c695fa1937770074e
nagoya313 <nagoya313@gmail.com>
2011-07-28 02:06:38 +0900

    取り敢えずコンストラクタ復活

--- ngy313/detail/opengl_types.hpp
+++ ngy313/detail/opengl_types.hpp
@@ -7,7 +7,7 @@
 #include <GL/gl.h>
 
 namespace ngy313 { namespace detail {
-struct opengl_fvf_type : private boost::noncopyable {
+struct opengl_fvf_type {
   typedef std::uint32_t value_type;
 
   static constexpr value_type kXYZ_RHW = 0x00;
@@ -17,11 +17,9 @@ struct opengl_fvf_type : private boost::noncopyable {
   static constexpr value_type kSpecular = 0x04;
   static constexpr value_type kTexture = 0x08;
   static constexpr value_type kMultiTexture = 0x10;
-  
-  opengl_fvf_type() = delete;
 };
 
-struct opengl_primitive_type : private boost::noncopyable {
+struct opengl_primitive_type {
   typedef GLenum value_type;
 
   static constexpr value_type kPointList = GL_POINTS;
@@ -30,11 +28,9 @@ struct opengl_primitive_type : private boost::noncopyable {
   static constexpr value_type kTriangleList = GL_TRIANGLES;
   static constexpr value_type kTriangleStrip = GL_TRIANGLE_STRIP;
   static constexpr value_type kTriangleFan = GL_TRIANGLE_FAN;
-
-  opengl_primitive_type() = delete;
 };
 
-struct opengl_blend_type : private boost::noncopyable {
+struct opengl_blend_type {
   typedef GLenum value_type;
 
   static constexpr value_type kBlendOne = GL_ONE;
@@ -47,8 +43,6 @@ struct opengl_blend_type : private boost::noncopyable {
   static constexpr value_type kBlendInvSrcColor = GL_ONE_MINUS_SRC_COLOR;
   static constexpr value_type kBlendDestColor = GL_DST_COLOR;
   static constexpr value_type kBlendInvDestColor = GL_ONE_MINUS_DST_COLOR;
-
-  opengl_blend_type() = delete;
 };
 }}
 
