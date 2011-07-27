9008d384b27ebc0fe1fcff879285c47f05ad7fb2
nagoya313 <nagoya313@gmail.com>
2011-07-28 01:57:43 +0900

    前方宣言を追加

--- ngy313/detail/fwd.hpp
+++ ngy313/detail/fwd.hpp
@@ -2,11 +2,17 @@
 #define NGY313_DETAIL_FWD_HPP_
 
 namespace ngy313 { namespace detail {
+template <typename Main>
+class basic_main;
+
 template <typename Window>
 class basic_window;
 
 template <typename Device>
 class basic_graphic_system;
+
+template <typename Sound>
+class basic_sound_system;
 }}
 
 #endif
