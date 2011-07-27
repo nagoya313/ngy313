5a53cc40b1f831d3912eb8f13d7e15a16f39666e
nagoya313 <nagoya313@gmail.com>
2011-07-28 02:05:31 +0900

    Loaderテンプレート引数を削除

--- ngy313/detail/voice.hpp
+++ ngy313/detail/voice.hpp
@@ -1,6 +1,7 @@
 #ifndef NGY313_DETAIL_VOICE_HPP_
 #define NGY313_DETAIL_VOICE_HPP_
 
+#include <cassert>
 #include <utility>
 #include <boost/noncopyable.hpp>
 
@@ -11,12 +12,12 @@
 #endif
 
 namespace ngy313 { namespace detail {
-template <typename Voice, typename Loader>
+template <typename Voice>
 class basic_voice : boost::noncopyable {
  public:
-  template <typename Device, typename Load>
-  explicit basic_voice(const Device &device, Load &&loader)
-      : voice_(device, std::forward<Load>(loader)) {}
+  template <typename Device, typename Loader>
+  explicit basic_voice(const Device &device, Loader &&loader, bool loop)
+      : voice_(device, std::forward<Loader>(loader), loop) {}
 
   void start() {
     voice_.start();
@@ -31,7 +32,8 @@ class basic_voice : boost::noncopyable {
   }
 
   void set_volume(float volume) {
-    voice_.setVolume(volume);
+    voice_.set_volume(volume);
+    assert(volume == this->volume());
   }
 
   float volume() const {
@@ -48,14 +50,11 @@ struct voice {
   typedef basic_voice<xaudio2_voice<Loader>, Loader> type;
 };
 #elif defined(__linux__)
-template <typename Loader>
-struct voice {
-  typedef basic_voice<openal_voice<Loader>, Loader> type;
-};
+typedef basic_voice<openal_voice> voice;
 
 template <typename Loader>
 struct streaming_voice {
-  typedef basic_voice<openal_streaming_voice<Loader>, Loader> type;
+  typedef basic_voice<openal_streaming_voice<Loader>> type;
 };
 #endif
 }}
